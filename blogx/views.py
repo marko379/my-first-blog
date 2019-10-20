78bcac41a6284dc8c100370c24e17763a6e5310d
marko379 <markogojkovic045@gmail.com>
2019-10-20 19:11:35 +0100

    Added view and template for detailed blog post as well as CSS for the site.

--- blogx/views.py
+++ blogx/views.py
@@ -4,11 +4,19 @@ Both views.py and models.py are in the same directory'''
 from django.shortcuts import render
 from .models import Post 
 from django.utils import timezone 
+from django.shortcuts import render, get_object_or_404
+
+
 
 
 def  post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #posts. posts. Treat this as the name of our QuerySet. 
 	return render (request,'blogx/post_list.html',{'posts': posts})
+	
+	
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blogx/post_detail.html', {'post': post})
 	
 
 '''As you can see, we created a function (def) called post_list that takes request and will return the value it gets from 
